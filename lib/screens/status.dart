a58c95683cc197220dedab9c7c2045045f1fc6b1
Mujhtech <mujeeb.muhideen@gmail.com>
2021-04-12 23:18:26 +0100

    add animation

--- lib/screens/status.dart
+++ lib/screens/status.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:thumbnails/thumbnails.dart';
@@ -47,100 +48,130 @@ class _MyStatusState extends State<MyStatus> {
         child: Directory('${_photoDir.path}').existsSync()
             ? Container(
                 margin: const EdgeInsets.only(left: 8, right: 8),
-                child: StaggeredGridView.countBuilder(
-                  itemCount: imageList.length,
-                  crossAxisCount: 4,
-                  itemBuilder: (context, index) {
-                    final filePath = imageList[index];
-                    return filePath.endsWith(".jpg")
-                        ? Material(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(8)),
-                            child: InkWell(
-                              onTap: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        ViewStatus(filePath: filePath),
-                                  )),
-                              child: Hero(
-                                  tag: filePath,
-                                  child: Image.file(
-                                    File(filePath),
-                                    fit: BoxFit.cover,
-                                  )),
-                            ),
-                          )
-                        : InkWell(
-                            onTap: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      ViewStatus(filePath: filePath),
-                                )),
-                            child: Stack(children: [
-                              ClipRRect(
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(12)),
-                                  child: Container(
-                                    decoration: const BoxDecoration(
-                                      gradient: LinearGradient(
-                                        begin: Alignment.bottomLeft,
-                                        end: Alignment.topRight,
-                                        stops: [0.1, 0.3, 0.5, 0.7, 0.9],
-                                        colors: [
-                                          Color(0xffb7d8cf),
-                                          Color(0xffb7d8cf),
-                                          Color(0xffb7d8cf),
-                                          Color(0xffb7d8cf),
-                                          Color(0xffb7d8cf),
-                                        ],
-                                      ),
+                child: AnimationLimiter(
+                  child: StaggeredGridView.countBuilder(
+                    physics: BouncingScrollPhysics(
+                        parent: AlwaysScrollableScrollPhysics()),
+                    itemCount: imageList.length,
+                    crossAxisCount: 4,
+                    itemBuilder: (context, index) {
+                      final filePath = imageList[index];
+                      return filePath.endsWith(".jpg")
+                          ? AnimationConfiguration.staggeredGrid(
+                              position: index,
+                              duration: const Duration(milliseconds: 500),
+                              columnCount: 2,
+                              child: ScaleAnimation(
+                                child: FadeInAnimation(
+                                  child: Material(
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(8)),
+                                    child: InkWell(
+                                      onTap: () => Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                ViewStatus(filePath: filePath),
+                                          )),
+                                      child: Hero(
+                                          tag: filePath,
+                                          child: Image.file(
+                                            File(filePath),
+                                            fit: BoxFit.cover,
+                                          )),
                                     ),
-                                    child: FutureBuilder(
-                                        future: _getImage(filePath),
-                                        builder: (context, snapshot) {
-                                          if (snapshot.connectionState ==
-                                              ConnectionState.done) {
-                                            if (snapshot.hasData) {
-                                              return Hero(
-                                                tag: filePath,
-                                                child: Image.file(
-                                                  File(snapshot.data),
-                                                  fit: BoxFit.cover,
-                                                ),
-                                              );
-                                            } else {
-                                              return const Center(
-                                                child:
-                                                    CircularProgressIndicator(),
-                                              );
-                                            }
-                                          } else {
-                                            return Hero(
-                                              tag: filePath,
-                                              child: SizedBox(
-                                                height: 280.0,
-                                                child: Image.asset(
-                                                    'assets/images/video_loader.gif'),
+                                  ),
+                                ),
+                              ),
+                            )
+                          : AnimationConfiguration.staggeredGrid(
+                              position: index,
+                              duration: const Duration(milliseconds: 500),
+                              columnCount: 2,
+                              child: ScaleAnimation(
+                                child: FadeInAnimation(
+                                  child: InkWell(
+                                    onTap: () => Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              ViewStatus(filePath: filePath),
+                                        )),
+                                    child: Stack(children: [
+                                      ClipRRect(
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(12)),
+                                          child: Container(
+                                            decoration: const BoxDecoration(
+                                              gradient: LinearGradient(
+                                                begin: Alignment.bottomLeft,
+                                                end: Alignment.topRight,
+                                                stops: [
+                                                  0.1,
+                                                  0.3,
+                                                  0.5,
+                                                  0.7,
+                                                  0.9
+                                                ],
+                                                colors: [
+                                                  Color(0xffb7d8cf),
+                                                  Color(0xffb7d8cf),
+                                                  Color(0xffb7d8cf),
+                                                  Color(0xffb7d8cf),
+                                                  Color(0xffb7d8cf),
+                                                ],
                                               ),
-                                            );
-                                          }
-                                        }),
-                                  )),
-                              Center(
-                                  child: Icon(
-                                FontAwesomeIcons.playCircle,
-                                size: 30,
-                                color: Theme.of(context).iconTheme.color,
-                              )),
-                            ]),
-                          );
-                  },
-                  staggeredTileBuilder: (i) =>
-                      StaggeredTile.count(2, i.isEven ? 2 : 3),
-                  mainAxisSpacing: 8.0,
-                  crossAxisSpacing: 8.0,
+                                            ),
+                                            child: FutureBuilder(
+                                                future: _getImage(filePath),
+                                                builder: (context, snapshot) {
+                                                  if (snapshot
+                                                          .connectionState ==
+                                                      ConnectionState.done) {
+                                                    if (snapshot.hasData) {
+                                                      return Hero(
+                                                        tag: filePath,
+                                                        child: Image.file(
+                                                          File(snapshot.data),
+                                                          fit: BoxFit.cover,
+                                                        ),
+                                                      );
+                                                    } else {
+                                                      return const Center(
+                                                        child:
+                                                            CircularProgressIndicator(),
+                                                      );
+                                                    }
+                                                  } else {
+                                                    return Hero(
+                                                      tag: filePath,
+                                                      child: SizedBox(
+                                                        height: 280.0,
+                                                        child: Image.asset(
+                                                            'assets/images/video_loader.gif'),
+                                                      ),
+                                                    );
+                                                  }
+                                                }),
+                                          )),
+                                      Center(
+                                          child: Icon(
+                                        FontAwesomeIcons.playCircle,
+                                        size: 30,
+                                        color:
+                                            Theme.of(context).iconTheme.color,
+                                      )),
+                                    ]),
+                                  ),
+                                ),
+                              ),
+                            );
+                    },
+                    staggeredTileBuilder: (i) =>
+                        StaggeredTile.count(2, i.isEven ? 2 : 3),
+                    mainAxisSpacing: 8.0,
+                    crossAxisSpacing: 8.0,
+                  ),
                 ),
               )
             : Text('No status found'),
